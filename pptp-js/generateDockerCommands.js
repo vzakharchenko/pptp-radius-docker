3a9364d9d8992194adc643fa8a720fe0942ead83
vzakharchenko <vzakharchenko@modeln.com>
2021-01-10 00:31:38 +0200

    fixed generateDockerCommands.js

--- pptp-js/generateDockerCommands.js
+++ pptp-js/generateDockerCommands.js
@@ -4,6 +4,19 @@ const configPath = process.env['CONFIG_PATH'] || '../examples/config.json';
 
 function parseFile(cJson) {
     let redir = '';
+    let radsec = '';
+    if (cJson.radsec) {
+        if (!fs.existsSync(cJson.radsec.privateKey)) {
+            throw new Error('Radsec Private key does not exist')
+        }
+        if (!fs.existsSync(cJson.radsec.certificateFile)) {
+            throw new Error('Radsec Public key does not exist')
+        }
+        if (!fs.existsSync(cJson.radsec.CACertificateFile)) {
+            throw new Error('Radsec CA key does not exist')
+        }
+        radsec = `-v ${cJson.radsec.privateKey}:${cJson.radsec.privateKey} -v ${cJson.radsec.certificateFile}:${cJson.radsec.certificateFile} -v ${cJson.radsec.CACertificateFile}:${cJson.radsec.CACertificateFile} `
+    }
     if (cJson.authorizationMap) {
         if (cJson.authorizationMap.roles) {
             Object.entries(cJson.authorizationMap.roles).forEach(((kv) => {
@@ -19,7 +32,7 @@ function parseFile(cJson) {
     }
 
     console.log('docker run -d --name=pptp-radius-docker -p 1723:1723 -p 3799:3799 ' + redir +
-        `-v ${configPath}:/opt/config.json --privileged --restart=always vassio/pptp-port-forwarding:latest`)
+        `-v ${configPath}:/opt/config.json ${radsec} --privileged --restart=always vassio/pptp-port-forwarding:latest`)
 }
 
 const f = fs.readFileSync(configPath, 'utf8');
